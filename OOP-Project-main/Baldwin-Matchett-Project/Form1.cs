6e144698384965e31b2eb3dd13e47497b0367696
parkermatchett <pmatchett02@mynbcc.ca>
2025-04-09 21:39:45 -0300

    DL THIS FOLDER + many changes
    Something weird happened with one of our commits and there were duplicate files, this should hopefully contain both of our changes and no duplicates
    
    
    
    - Altered Forms to fit data better, added logo
    - Login process now uses ValidateUser and the filled user list instead of switch case
    - Cleaned up frmOrder displaying, still one unknown issue with displaying the extra info
    - Adding products to cart now works and catches any errors that may come up

---
+++ OOP-Project-main/Baldwin-Matchett-Project/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baldwin_Matchett_Project
+{
+    public partial class frmLogin : Form
+    {
+
+
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userIn = txtUser.Text;
+            string passIn = txtPassword.Text;
+
+            List<User> userlist = new List<User>();
+            FileHelper.ReadUsers("users.txt", userlist);
+
+
+            if (Validator.ValidateUser(userlist, userIn, passIn, out User loginUser))
+            {
+                frmOrder order = new frmOrder(loginUser);
+                order.ShowDialog();
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ OOP-Project-main/Baldwin-Matchett-Project/frmOrder.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baldwin_Matchett_Project
+{
+    public partial class frmOrder : Form
+    {
+        Inventory i = new Inventory();
+        private Cart c = new Cart();
+        string username;
+        string access;
+
+        public frmOrder(User user)
+        {
+            InitializeComponent();
+            username = user.Name;
+            access = user.Access;
+
+        }
+
+        private void frmOrder_Load(object sender, EventArgs e)
+        {
+            // Hides the tab headers
+            tabOrders.Appearance = TabAppearance.FlatButtons;
+            tabOrders.ItemSize = new Size(0, 1);
+            tabOrders.SizeMode = TabSizeMode.Fixed;
+
+            if (access == "admin")
+            {
+                tabOrders.SelectedIndex = 1;
+            }
+            if (access == "customer")
+            {
+
+                tabOrders.SelectedIndex = 2;
+                i.HideZeroes(lstProducts); //hide unavailable items if in customer view
+
+            }
+
+
+            //testing file reading vvv
+            // this reads the file, we may want to simplify it so that
+            // the left lstBox reads a code, name, qty, and furhter information
+            // displays while selected
+            FileHelper.ReadProducts("inventory.txt", i.inventory);
+            i.UpdateListBox(lstProducts);
+
+
+
+        }
+
+
+        private void btnPurchase_Click(object sender, EventArgs e)
+        {
+
+            int qty = (int)nudQuantity.Value;
+
+
+
+            //add selected item to cart
+            try
+            {
+                int selectedQty = i.inventory[lstProducts.SelectedIndex].Quantity;
+                if (selectedQty > 0)
+                {
+                    if (selectedQty >= qty)
+                    {
+                        for (int n = qty; n > 0; n--)
+                        {
+                            c.cart.Add(i.inventory[lstProducts.SelectedIndex]);
+                            c.count++;
+                            i.inventory[lstProducts.SelectedIndex].Quantity--;
+                            lblItemsInCart.Text = $"Items in Cart: {c.count}";
+                            lblCost.Text = $"$ {c.TotalCart()}";
+                        }
+                        i.UpdateListBox(lstProducts);
+                    }
+                    else
+                    {
+                        MessageBox.Show("That amount is not available to order", "err");
+                    }
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("No item selected", "err");
+            }
+
+
+
+
+
+        }
+
+
+        private void btnViewCart_Click(object sender, EventArgs e)
+        {
+            frmCheckout cart = new frmCheckout(c);
+            cart.ShowDialog();
+        }
+
+        private void lstProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // get currently selected product
+            // determine its type
+            // then fill out further information
+
+
+            if (i.inventory[lstProducts.SelectedIndex] is AntiqueFurniture)
+            {
+                AntiqueFurniture selected = (AntiqueFurniture)i.inventory[lstProducts.SelectedIndex];
+                lblDescription.Text =  $"Item Code:   {selected.Code}\n";
+                lblDescription.Text += $"Price:       ${selected.Price}\n";
+                lblDescription.Text += $"Creator:     {selected.Creator}\n";
+                lblDescription.Text += $"Origin:      {selected.Origin}";
+            }
+            if (i.inventory[lstProducts.SelectedIndex] is VintageJewelry)
+            {
+                VintageJewelry selected = (VintageJewelry)i.inventory[lstProducts.SelectedIndex];
+                lblDescription.Text =  $"Item Code:   {selected.Code}\n";
+                lblDescription.Text += $"Price:       ${selected.Price}\n";
+                lblDescription.Text += $"Material:    {selected.Metal}\n";
+                lblDescription.Text += $"Age:         {selected.Age} Years";
+            }
+        }
+
+        /*
+         *  Admin controls
+         */ 
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnEditProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemoveProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
